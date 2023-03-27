e0fd4b1671eb66dbe5bac3c0545033e77baeb36c
LUCAS PEREIRA DE SOUZA <lucas.psouza27@senacsp.edu.br>
2023-03-27 16:47:03 -0300

    adição de um novo arquivo teste

---
+++ Untitled1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <math.c>
+using namespace 
+
+int main (){
+	int contexto;
+	
+	switch (contexto)
+	{
+	case 1:
+		cout << "teste"
+		break;
+	
+	default:
+
+		break;
+	}
+}
